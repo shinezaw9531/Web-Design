73a5a7946428de84c8cee02712cfbf72d0900e7b
shinezaw2c2p <shinezaw@2c2p.com>
2023-04-21 11:24:32 +0630

    Random Color Palette Generator Project

---
+++ Random Color Palette Generator/script.js
@@ -0,0 +1,30 @@
+const container = document.querySelector(".container");
+const refreshBtn = document.querySelector(".refresh-btn");
+
+const maxPaletteBoxes = 32;
+
+const genereatePalette = () => {
+    container.innerHTML = "";
+    for(let i=0; i<maxPaletteBoxes; i++){
+        let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
+        randomHex = `#${randomHex.padStart(6,"0")}`; // 12345 => 012345
+
+        const color = document.createElement("li");
+        color.classList.add("color");
+        color.innerHTML = `<div class="rect-box" style="background: ${randomHex}"></div>
+                            <span class="hex-value">${randomHex}</span>`;
+        color.addEventListener("click", () => copyColor(color, randomHex));
+        container.appendChild(color);
+    }
+
+}
+genereatePalette();
+const copyColor = (elem, hexVal) => {
+    const colorElement = elem.querySelector(".hex-value");
+
+    navigator.clipboard.writeText(hexVal).then(() => {
+        colorElement.innerHTML = "Copied";
+        setTimeout(() => colorElement.innerHTML = hexVal, 1000);
+    }).catch(() => alert("Failed to copy the color code!"));
+}
+refreshBtn.addEventListener("click", genereatePalette);
